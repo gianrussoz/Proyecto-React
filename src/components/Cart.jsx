4dbeda4c985d0f3659fe9e0824d1257a527c19a5
gianrussoz <gianfranco25032@gmail.com>
2022-04-27 00:24:31 -0300

    Problema solucionado

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,5 +1,6 @@
 import { useContext } from 'react';
 import { CartContext } from '../context/CartContext';
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
     const test = useContext(CartContext);
@@ -7,16 +8,37 @@ const Cart = () => {
     return (
         <>
             <h2>Tu carrito</h2>
-            <button onClick={test.removeList}>Vaciar carrito</button>
+            <Link to='/'><button>Seguir comprando</button></Link>
+                {
+                    (test.cartList.length > 0)
+                    ? <button type="filled" onClick={test.borrarLista}>Vaciar carrito</button>
+                    : <p>Your cart is empty</p>
+                }
+
             {  test.cartList.map(item =>
                 <div>
                     <img alt="foto" src={item.img} />
                     <span>
-                        <b>Product: {item.name}</b>
+                        <b>Producto: {item.name}</b>
                     </span>
                     <div>{item.precio}</div>
+                    <p>$ {test.precioItem(item.id)}</p>
                 </div>
             )
+            }
+
+            {
+                test.cartList.length > 0 &&
+                <div>
+                    <h4>ORDER SUMMARY</h4>
+                        <p>Subtotal</p>
+                        <strong>{test.calcSubTotal()}</strong>
+                        <p>IVA</p>
+                        <strong>{test.iva()}</strong>
+                        <p>Total</p>
+                        <strong>{test.precioFinal()}</strong>
+                    <p>Terminar compra</p>
+                </div>
             }
         </>
     );
--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
+import { useContext } from 'react';
 import Badge from '@mui/material/Badge';
 import IconButton from '@mui/material/IconButton';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import { CartContext } from '../context/CartContext';
 
 const CartWidget = () => {
+    const test = useContext(CartContext);
+
     return (
         <>
             <IconButton aria-label="cart">
-                <Badge badgeContent={1} color="primary">
+                <Badge badgeContent={test.itemsCW()} color="primary">
                     <ShoppingCartIcon  className="botonCarrito"/>
                 </Badge>
             </IconButton>
         </>
     )
 }
-export default CartWidget
+
+export default CartWidget;
